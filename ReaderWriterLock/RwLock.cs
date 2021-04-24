669cff67a6226704f3a5ee0a9564d369108db0c5
BandLComplete <boroviki2000@mail.ru>
2021-04-24 14:07:09 +0500

    Рефакторинг RwLock

--- ReaderWriterLock/RwLock.cs
+++ ReaderWriterLock/RwLock.cs
@@ -7,47 +7,32 @@ namespace ReaderWriterLock
 	public class RwLock : IRwLock
 	{
 		private readonly HashSet<object> _locks = new HashSet<object>();
-		private readonly object _lockIn = new object();
 	
 		public void ReadLocked(Action action)
 		{
 			var obj = new object();
-			lock (obj)
-			{
-				lock (_lockIn)
-				{
-					lock (_locks)
-						_locks.Add(obj);
-				}
 			
+			lock (_locks)
+				_locks.Add(obj);
+
+			lock (obj)
 				action();
-				
-				lock (_locks)
-				{
-					_locks.Remove(obj);
-					if (_locks.Count == 0) 
-						Monitor.Pulse(_locks);
-				}
-			}
+			
+			lock (_locks)
+				_locks.Remove(obj);
 		}
 
 		public void WriteLocked(Action action)
 		{
-			lock (_lockIn)
+			lock (_locks)
 			{
-				lock (_locks)
-				{
-					if (_locks.Count > 0)
-						Monitor.Wait(_locks);
-					
-					foreach (var obj in _locks)
-						Monitor.Enter(obj);
+				foreach (var obj in _locks)
+					Monitor.Enter(obj);
 
-					action();
+				action();
 				
-					foreach (var obj in _locks)
-						Monitor.Exit(obj);
-				}
+				foreach (var obj in _locks)
+					Monitor.Exit(obj);
 			}
 		}
 	}
